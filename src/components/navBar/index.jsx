0f610549e0df1381c51804cfcae0f22f47e0eac4
SnehaDileep <dileepsneha99@gmail.com>
2022-11-01 20:58:36 +0100

    edited

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import {HiX } from "react-icons/hi";
 import { FaBars, FaReact } from "react-icons/fa";
-import { HiX } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import "./styles.scss";
 const data = [
@@ -24,35 +24,37 @@ const data = [
 ];
 
 const Navbar = () => {
-  const [toggleIcon, setToggleIcon] = useState(false);
+  const [click, setClick] = useState(false);
 
-  const handleToggleIcon = () => {
-    setToggleIcon(!toggleIcon);
-  };
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
+
   return (
     <div>
-      <nav className="navbar">
+      <div className={click ? "main-container" : ""} onClick={() => Close()} />
+      <nav className="navbar" onClick={(e) => e.stopPropagation()}>
         <div className="navbar__container">
-          <Link to={"/"} className="navbar__container__logo">
+          <Link exact to="/" className="navbar__container__logo">
             <FaReact size={30} />
           </Link>
-        </div>
-        <ul
-          className={`navbar__container__menu ${toggleIcon ? "active" : ""} `}
-        >
-          {data.map((item, key) => (
-            <li key={key} className="navbar__container__menu__item">
-              <Link
-                className="navbar__container__menu__item__links"
-                to={item.to}
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="nav-icon" onClick={handleToggleIcon}>
-          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+          <ul className={click ? "navbar__container__menu active" : "navbar__container__menu"}>
+            {data.map((item) => (
+              <li className="navbar__container__menu__item">
+                <Link
+                  exact
+                  to={item.to}
+                  activeClassName="active"
+                  className="navbar__container__menu__item__links"
+                  onClick={click ? handleClick : null}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {click ? <HiX size={30} /> : <FaBars size={30} />}
+          </div>
         </div>
       </nav>
     </div>
